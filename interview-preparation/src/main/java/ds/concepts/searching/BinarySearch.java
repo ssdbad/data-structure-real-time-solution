a00793bb717f21fe34bb6a96e3ffc9ab3f76a590
Gauta Raj <Gautam.Raj@quintiles.com>
2021-07-08 13:12:36 +0530

    Binary Search Algorithm is implemented by iterative logic

--- interview-preparation/src/main/java/ds/concepts/searching/BinarySearch.java
+++ interview-preparation/src/main/java/ds/concepts/searching/BinarySearch.java
@@ -1,5 +1,35 @@
 package ds.concepts.searching;
 
 public class BinarySearch {
+    /**
+     * Iterative way of binary search
+     * @param array
+     * @param key
+     * @return element found on index 
+     */
+    public final static int binarySearch(int[] array, int key){
+        int lowerIndex = 0;
+        int rightIndex = array.length-1;
+        while (lowerIndex < rightIndex) {
+            int midIndex = lowerIndex + (rightIndex-1)/2;
 
+            // check KEY value is present at middle index
+            if(array[midIndex] == key){
+                return midIndex;
+            }
+
+            // if KEY is greater than middleIndex value then ignore left half values.
+            if(array[midIndex] < key){
+                lowerIndex = midIndex + 1;
+            }
+
+            // if KEY is smaller than middleIndex value than inore right half values.
+            if(array[midIndex] > key){
+                rightIndex = midIndex-1;
+            }
+
+        }
+        //return -1, f element is not found in the given array.
+        return -1;
+    }
 }
--- interview-preparation/src/main/java/ds/linkedList/concepts/_JavaArrayList.java
+++ interview-preparation/src/main/java/ds/linkedList/concepts/_JavaArrayList.java
@@ -1,5 +1,13 @@
 package ds.linkedList.concepts;
 
+import java.util.ArrayList;
 public class _JavaArrayList {
-    
+ public static void main(String[] args) {
+     ArrayList<Object> list = new ArrayList<>();
+     list.add("Gautam");
+     list.add("Raj");
+     list.add("Sonu");
+     list.removeIf( x -> x.equals("Sonu"));
+     System.out.println(list);
+ }   
 }
